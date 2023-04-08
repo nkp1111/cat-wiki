385b4ffe81da870242a22913bbbb1998aab5e65f
nkp1111 <neerajp4321@gmail.com>
2023-04-08 12:29:34 +0530

    feat: add cat image grid

---
+++ src/components/Info/ImageGrid.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { images } from '../../utils'
+
+const { image1, image2, image3 } = images
+const ImageGrid = () => {
+  return (
+    <div className='info-grid'>
+      <div className="d-flex gap-3">
+        <div className="d-flex flex-column align-items-end gap-3">
+          <div className='img-holder-1'>
+            <img src={image2} alt="cat" />
+          </div>
+          <div className='img-holder-3'>
+            <img src={image1} alt="cat" />
+          </div>
+        </div>
+        <div className='img-holder-2'>
+          <img src={image3} alt="cat" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ImageGrid
